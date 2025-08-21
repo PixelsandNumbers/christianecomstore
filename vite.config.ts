2533389c06f35c3ca3dd93ac7ffddc85c306901a
PixelsandNumbers <nilsgoldingers@pixelsandnumbers.com>
2025-08-21 19:39:51 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -11,9 +9,8 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    ...(mode === "development" ? [require("lovable-tagger").componentTagger()] : []),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
